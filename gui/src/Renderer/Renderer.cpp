b8799d55ff3fb64459d76992bb34b3dadefb8a69
Mateo <mateo.antoni-christ@epitech.eu>
2026-06-28 14:17:05 +0200

    fix(gui): fixed the rendering of the ressources by drawing them at right time

--- gui/src/Renderer/Renderer.cpp
+++ gui/src/Renderer/Renderer.cpp
@@ -5,12 +5,12 @@ Renderer::Renderer(AssetManager& assets) : _assets(assets) {}
 
 void Renderer::drawMap(const Map& map, const CharacterFactory& factory, const EggFactory& eggs, PlayerView& camera)
 {
-    _drawTiles(map);
+    _drawTiles(map, camera);
     _drawEggs(eggs, camera);
     _drawCharacters(factory, camera);
 }
 
-void Renderer::_drawTiles(const Map& map)
+void Renderer::_drawTiles(const Map& map, PlayerView& camera)
 {
     for (int y = 0; y < map.getHeight(); y++) {
         for (int x = 0; x < map.getWidth(); x++) {
@@ -20,11 +20,18 @@ void Renderer::_drawTiles(const Map& map)
                 0.f,
                 y * TILE_SIZE + TILE_SIZE / 2.f
             };
-            Color color = (tile.q0 > 0) ? GREEN :
-                          (tile.q1 > 0) ? YELLOW :
-                          DARKGREEN;
+            Color color = DARKGREEN;
             DrawCube(pos, TILE_SIZE, 0.1f, TILE_SIZE, color);
             DrawCubeWires(pos, TILE_SIZE, 0.1f, TILE_SIZE, BLACK);
+
+            Vector3 resPos = { pos.x, 0.15f, pos.z };
+            if (tile.q0 > 0) DrawBillboard(camera.get(), _assets.get("food"),       resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q1 > 0) DrawBillboard(camera.get(), _assets.get("GoldOre"),    resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q2 > 0) DrawBillboard(camera.get(), _assets.get("IronOre"),    resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q3 > 0) DrawBillboard(camera.get(), _assets.get("Stone"),      resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q4 > 0) DrawBillboard(camera.get(), _assets.get("Fang"),       resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q5 > 0) DrawBillboard(camera.get(), _assets.get("Ruby"),       resPos, TILE_SIZE * 0.4f, WHITE);
+            if (tile.q6 > 0) DrawBillboard(camera.get(), _assets.get("Starshard"),  resPos, TILE_SIZE * 0.4f, WHITE);
         }
     }
 }
