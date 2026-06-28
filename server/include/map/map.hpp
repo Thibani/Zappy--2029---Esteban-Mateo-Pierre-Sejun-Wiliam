17349b373b9c7c483058a231d6c7c7e10ac513e4
William Wagner <williamwagner@epitech.eu>
2026-06-28 19:08:06 +0200

    fix(map): include path and update setRessource return type in Map class

--- server/include/map/map.hpp
+++ server/include/map/map.hpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 #include <vector>
 #include <string>
-#include "../types/position.hpp"
+#include "types/position.hpp"
 
 
 namespace Zappy {
@@ -17,7 +17,7 @@ namespace Zappy {
             Map(int width, int height);
             ~Map();
             Tile* getTile(Pos position);
-            void setRessource();
+            std::vector<Pos> setRessource();
             int getHeight() { return _height; }
             int getWidth() { return _width; }
             void correctPos(Pos *position);
@@ -26,10 +26,10 @@ namespace Zappy {
             Player* eggHatching(Egg* egg, const std::string &teamName);
 
         private:
-            std::vector<std::vector<Tile*>> _tiles;
-            int _height;
-            int _width;
-            float _densities[7] = {0.5, 0.3, 0.15, 0.1, 0.1, 0.08, 0.05};
-            std::vector<int> countResources();
+        std::vector<std::vector<Tile*>> _tiles;
+        int _height;
+        int _width;
+        float _densities[7] = {0.5, 0.3, 0.15, 0.1, 0.1, 0.08, 0.05};
+        std::vector<int> countResources();
     };
 }
